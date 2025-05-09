f267c13687f0572162d464981b4ff86000caa573
djeytq <djeytiquimica@gmail.com>
2025-05-09 03:57:45 +0300

    fix

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres', // Substitua pelo seu usuário do PostgreSQL
-    host: 'localhost', // Ou o host do seu banco de dados
+    user: 'biblioteca_owner', // Substitua pelo seu usuário do PostgreSQL
+    host: 'ep-shrill-river-a45t94k7-pooler.us-east-1.aws.neon.tech', // Ou o host do seu banco de dados
     database: 'biblioteca', // Substitua pelo nome do seu banco de dados
-    password: '0000', // Substitua pela sua senha
+    password: 'npg_nlJPUECM3S0b', // Substitua pela sua senha
     // port: 5432, // Porta padrão do PostgreSQL
      ssl: {
             rejectUnauthorized: false // Adicione esta linha para usar SSL
